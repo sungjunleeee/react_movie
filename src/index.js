009d11f9048fad80b4499d92985d22aec0a83ab5
Sungjun Lee <smart5597@naver.com>
2021-05-18 14:33:45 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import State from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <State />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
